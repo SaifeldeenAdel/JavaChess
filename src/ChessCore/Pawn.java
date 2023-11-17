ad5b46dd513380084232c32179c0961aa87f4bc5
Asmaa <es-asmaa.ghanem2026@alexu.edu.eg>
2023-11-17 14:59:18 +0200

    Added changes to pawn, working on special moves

--- src/ChessCore/Pawn.java
+++ src/ChessCore/Pawn.java
@@ -21,16 +21,61 @@ public class Pawn extends Piece {
         int vertical = this.isWhite() ? squareFrom.rank - squareTo.rank : squareTo.rank - squareFrom.rank;
 
         if(vertical == 1 && horizontal == 0){
+            this.hasMoved=true;
             return true;
         }
-        if(vertical == 2 && horizontal == 0 && !hasMoved){
+        if(vertical == 2 && horizontal == 0 && !hasMoved) {
+            this.hasMoved = true;
+            this.justMovedTwoSquares = true;
+
             return true;
         }
+        if(horizontal==1 && vertical ==-1){
+            if(squareTo.getPiece()!=null)
+                if(squareFrom.getPiece().isWhite()) {
+                    return false;
+                }
+                else{
+                    return true;
+                }
+        }
+
+
         if(horizontal == 1 && vertical == 1){
-            return true;
+            this.hasMoved =true;
+            if(squareTo.getPiece()!=null)
+            {
+                if(!squareFrom.getPiece().isWhite())
+                {
+                    return false;
+                }
+                else{
+                    return true;
+                }
+            }
+            else{
+
+               /* if((this.getBoard().getSquare(squareFrom.rank, squareFrom.file + 1).getPiece() != null) || this.getBoard().getSquare(squareFrom.rank, squareFrom.file - 1).getPiece() != null)
+                {
+                    //check that this piece is a pawn
+                    //if yes
+                    //
+                }*/
+
+
+
+            }
+
         }
+
+
         return false;
     }
+
+   /* private boolean checkSquareForPawn(Square squareFrom, Square squareTo)
+    {
+
+    }*/
 
     @Override
     public ArrayList<Square> getAllLegalMoves() {
