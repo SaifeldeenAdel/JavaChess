377cfd3166c187e7a19f476d3c4a3f6440082d68
Saif eldeen Adel <saifadel77@hotmail.com>
2023-11-20 11:35:27 +0200

    Fixed board, forgot to uncomment

--- src/ChessCore/Board.java
+++ src/ChessCore/Board.java
@@ -17,29 +17,29 @@ public class Board implements Cloneable{
     }
 
     public void initialisePieces(){
-//        for(int i = 0; i< Constants.BOARD_WIDTH; i++){
-//            squares[1][i].setPiece(new Pawn(this, squares[1][i], Color.WHITE));
-//            squares[6][i].setPiece(new Pawn(this, squares[6][i], Color.BLACK));
-//        }
+        for(int i = 0; i< Constants.BOARD_WIDTH; i++){
+            squares[1][i].setPiece(new Pawn(this, squares[1][i], Color.WHITE));
+            squares[6][i].setPiece(new Pawn(this, squares[6][i], Color.BLACK));
+        }
         // White Pieces
-//        squares[0][0].setPiece(new Rook(this, squares[0][0], Color.WHITE));
-//        squares[0][1].setPiece(new Knight(this, squares[0][1], Color.WHITE));
-//        squares[0][2].setPiece(new Bishop(this, squares[0][2], Color.WHITE));
-//        squares[0][3].setPiece(new Queen(this, squares[0][3], Color.WHITE));
+        squares[0][0].setPiece(new Rook(this, squares[0][0], Color.WHITE));
+        squares[0][1].setPiece(new Knight(this, squares[0][1], Color.WHITE));
+        squares[0][2].setPiece(new Bishop(this, squares[0][2], Color.WHITE));
+        squares[0][3].setPiece(new Queen(this, squares[0][3], Color.WHITE));
         squares[0][4].setPiece(new King(this, squares[0][4], Color.WHITE));
-//        squares[0][5].setPiece(new Bishop(this, squares[0][5], Color.WHITE));
+        squares[0][5].setPiece(new Bishop(this, squares[0][5], Color.WHITE));
         squares[0][6].setPiece(new Knight(this, squares[0][6], Color.WHITE));
-//        squares[0][7].setPiece(new Rook(this, squares[0][7], Color.WHITE));
+        squares[0][7].setPiece(new Rook(this, squares[0][7], Color.WHITE));
 
         // Black Pieces
-//        squares[7][0].setPiece(new Rook(this, squares[7][0], Color.BLACK));
+        squares[7][0].setPiece(new Rook(this, squares[7][0], Color.BLACK));
         squares[7][1].setPiece(new Knight(this, squares[7][1], Color.BLACK));
-//        squares[7][2].setPiece(new Bishop(this, squares[7][2], Color.BLACK));
-//        squares[7][3].setPiece(new Queen(this, squares[7][3], Color.BLACK));
+        squares[7][2].setPiece(new Bishop(this, squares[7][2], Color.BLACK));
+        squares[7][3].setPiece(new Queen(this, squares[7][3], Color.BLACK));
         squares[7][4].setPiece(new King(this, squares[7][4], Color.BLACK));
-//        squares[7][5].setPiece(new Bishop(this, squares[7][5], Color.BLACK));
-//        squares[7][6].setPiece(new Knight(this, squares[7][6], Color.BLACK));
-//        squares[7][7].setPiece(new Rook(this, squares[7][7], Color.BLACK));
+        squares[7][5].setPiece(new Bishop(this, squares[7][5], Color.BLACK));
+        squares[7][6].setPiece(new Knight(this, squares[7][6], Color.BLACK));
+        squares[7][7].setPiece(new Rook(this, squares[7][7], Color.BLACK));
 
 //        squares[2][1].setPiece(new Pawn(this, squares[2][1], Color.WHITE));
 //    //        squares[2][4].setPiece(new Queen(this, squares[2][4], Color.BLACK));
